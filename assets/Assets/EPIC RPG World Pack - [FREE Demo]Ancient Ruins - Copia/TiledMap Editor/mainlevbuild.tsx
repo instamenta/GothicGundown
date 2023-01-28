c1bc118fa3d9268d18b380f127c57a693b446ab4
instamenta <98179343+instamenta@users.noreply.github.com>
2023-01-28 05:29:05 +0200

    miracle

---
+++ assets/Assets/EPIC RPG World Pack - [FREE Demo]Ancient Ruins - Copia/TiledMap Editor/mainlevbuild.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="mainlevbuild" tilewidth="32" tileheight="32" tilecount="640" columns="32">
+ <image source="../../../TileSet/mainlevbuild.png" width="1024" height="640"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="279" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="280" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="281" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="311" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="312" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="313" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
